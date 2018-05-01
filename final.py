41a4f4d3a7aca62f12d3418ab9b2e1349c378479
JY <lovedct_jy@naver.com>
2018-05-01 22:00:28 +0900

    2D finish_final.py

---
+++ final.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+import matplotlib.pyplot as plt
+import networkx as nx
+import random
+import pdb
+
+
+###### G[i][i+1]['  '] -> G[Pu[i]][Pu[i+1]]['   ']
+###### sum_rec(Pl, Pu, S) -> sum_rec(Pu, Pl, S)
+def sum_rec(Pu, Pl,S):
+    #print('function called')
+    #print("now S is ",S)
+    rPl = 0; lPl = 0; rPu = 0; lPu = 0
+    for i in range(len(Pu)-1):
+            rPu += G[Pu[i]][Pu[i+1]]['risk']
+            lPu += G[Pu[i]][Pu[i+1]]['len']
+    for j in range(len(Pl)-1):
+            rPl += G[Pl[j]][Pl[j+1]]['risk']
+            lPl += G[Pl[j]][Pl[j+1]]['len']
+    if lPu == lPl : 
+        if lPl not in S : 
+            S = S+[Pl]
+        return S
+    pm = (rPu-rPl)/(lPu-lPl)
+    #print('rPu:',rPu,'lPu:', lPu,'rPl:', rPl, 'lPl:',lPl,'pm:', pm)
+    for (u,v,w) in G.edges(data=True):
+            w['f']=w['risk']-pm*w['len']
+    ### 여기에서 'f'값을 기준으로 Pi를 뽑을 건데,
+    ### 그 Pi가 Pl보다는 위에 있고 Pu보다는 아래에 존재한다는 "확신"을 'f'값이 주지 못하는 상태
+    ### G의 subG로 Pl,Pu가 만드는 사각형 안에 존재하는 P의 합(?)을 두고,
+    ### 전달 인자로 S말고 subG를 줘야 할 듯..>?
+    Pi = list(nx.dijkstra_path(G,s,t,weight='f'))
+    #print('Pi = ',Pi)
+    if Pi not in S:
+            #print("before insert Pi, S = ",S)
+            S = S+[Pi]
+            #print("after insert Pi, S = ",S)
+            sum_rec(Pu, Pi,S)
+            sum_rec(Pi, Pl,S)
+    #print('function return')
+    return S
+
+
+
+# 70 nodes, 280 edges random graph
+G = nx.read_gpickle("Graph_70_300.gpickle")
+
+# setting s_source node & t_target node
+s = int(input("sorce:"))
+t = int(input("target:")) 
+
+# S_set of paths from s to t
+S = []
+
+# Pl_shortest "LEN" path from s to t
+# Pu_shortest "RISK" path from s to t
+# x-coordinate : len / y-coordinate : risk
+# then, Pl pointed at "LOWER" position
+# and PU pointed at "UPPER" position
+##### Pl = len, Pu = risk -> Pl = risk, Pu = len
+Pl_ = nx.dijkstra_path(G,s,t,weight='len') #shortest path
+print("Pl_ = ",Pl_)
+Pr_ = nx.dijkstra_path(G,s,t,weight='risk') #safest path
+print("Pr_ = ",Pr_)
+
+S += [Pl_]
+if Pl_ != Pr_ : 
+    S += [Pr_]
+    S = sum_rec(Pl_, Pr_, S)
+
+#pdb.set_trace()
+print(len(S))
+print("Final result S : ",S)
