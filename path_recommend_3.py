6561d4add64b6d4423fba898bb1622f1caf4f5d5
JY <lovedct_jy@naver.com>
2018-05-03 13:56:07 +0900

    오류 수정_sum_rec() 파라미터 변경

--- path_recommend_3.py
+++ path_recommend_3.py
@@ -8,16 +8,16 @@ import pdb
 
 ###### G[i][i+1]['  '] -> G[Pu[i]][Pu[i+1]]['   ']
 ###### sum_rec(Pl, Pu, S) -> sum_rec(Pu, Pl, S)
-def sum_rec(Pu, Pl,S):
+def sum_rec(Pu, Pl,S, p1, p2):
     print('function called')
     print("now S is ",S)
     rPl = 0; lPl = 0; rPu = 0; lPu = 0
     for i in range(len(Pu)-1):
-            rPu += G[Pu[i]][Pu[i+1]]['risk']
-            lPu += G[Pu[i]][Pu[i+1]]['len']
+            rPu += G[Pu[i]][Pu[i+1]][p1]
+            lPu += G[Pu[i]][Pu[i+1]][p2]
     for j in range(len(Pl)-1):
-            rPl += G[Pl[j]][Pl[j+1]]['risk']
-            lPl += G[Pl[j]][Pl[j+1]]['len']
+            rPl += G[Pl[j]][Pl[j+1]][p1]
+            lPl += G[Pl[j]][Pl[j+1]][p2]
     if lPu == lPl : 
         if lPl not in S : 
             S = S+[Pl]
@@ -25,7 +25,7 @@ def sum_rec(Pu, Pl,S):
     pm = (rPu-rPl)/(lPu-lPl)
     print('rPu:',rPu,'lPu:', lPu,'rPl:', rPl, 'lPl:',lPl,'pm:', pm)
     for (u,v,w) in G.edges(data=True):
-            w['f']=w['risk']-pm*w['len']
+            w['f']=w[p1]-pm*w[p2]
     ### 여기에서 'f'값을 기준으로 Pi를 뽑을 건데,
     ### 그 Pi가 Pl보다는 위에 있고 Pu보다는 아래에 존재한다는 "확신"을 'f'값이 주지 못하는 상태
     ### G의 subG로 Pl,Pu가 만드는 사각형 안에 존재하는 P의 합(?)을 두고,
@@ -36,8 +36,8 @@ def sum_rec(Pu, Pl,S):
             #print("before insert Pi, S = ",S)
             S = S+[Pi]
             #print("after insert Pi, S = ",S)
-            S = sum_rec(Pu, Pi,S)
-            S = sum_rec(Pi, Pl,S)
+            S = sum_rec(Pu, Pi,S,p1,p2)
+            S = sum_rec(Pi, Pl,S,p1,p2)
     print('function return')
     return S
 
@@ -69,15 +69,18 @@ print("Pn_ = ",Pn_)
 S += [Pl_]
 if Pl_ != Pr_ : 
     S += [Pr_]
-    S = sum_rec(Pl_, Pr_, S)
+    S = sum_rec(Pl_, Pr_, S, 'risk', 'len')
 
 if Pn_ != Pr_ : 
     if Pn_ not in S :
-	    S += [Pn_]
-    S = sum_rec(Pn_, Pr_, S)
+        S += [Pn_]
+    S = sum_rec(Pn_, Pr_, S, 'risk', 'new')
 
 if Pn_ != Pl_ : 
-    S = sum_rec(Pn_, Pl_, S)
+    if Pn_ not in S :
+        S += [Pn_]
+    S = sum_rec(Pl_, Pn_, S, 'len', 'new')
+
 
 #pdb.set_trace()
 print(len(S))
